e01d67504e48468967c2ee184ab1503d7c2f347c
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-03-27 17:21:35 +1100

    Add request handling to activate gate/garage remotes

--- server/server.ino
+++ server/server.ino
@@ -7,13 +7,59 @@ constexpr char WIFI_PW[]{SECRET_WIFI_PW};
 constexpr int SERVER_PORT{80};
 WiFiServer server(SERVER_PORT);
 
+constexpr pin_size_t GATE_PIN{3};
+constexpr pin_size_t GARAGE_PIN{8};
+constexpr int ON_TIME{500}; // Duration gate/garage remote is on when activated
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(GATE_PIN, OUTPUT);
+  pinMode(GARAGE_PIN, OUTPUT);
 
   startServer();
 }
 
-void loop() {}
+void loop() {
+  WiFiClient client{server.available()};
+
+  if (client) {
+    Serial.println("Client connected");
+    Serial.println();
+
+    String request{""};
+
+    while (client.connected()) {
+      if (client.available()) {
+        request += static_cast<char>(client.read());
+      }
+
+      // If reached end of request header
+      if (request.endsWith("\r\n\r\n")) {
+        Serial.print(request);
+
+        // Respond to request
+        client.println("HTTP/1.0 204 No Content");
+        client.println();
+
+        break;
+      }
+    }
+
+    if (request.startsWith("GET /gate ")) {
+      powerOnThenOff(GATE_PIN, ON_TIME);
+    }
+
+    if (request.startsWith("GET /garage ")) {
+      powerOnThenOff(GARAGE_PIN, ON_TIME);
+    }
+
+    client.stop();
+
+    Serial.println("Client disconnected");
+    Serial.println();
+  }
+}
 
 void startServer() {
   if (WiFi.status() == WL_NO_MODULE) {
@@ -49,4 +95,10 @@ void startServer() {
   Serial.print("Server running at http://");
   Serial.println(WiFi.localIP());
   Serial.println();
+}
+
+void powerOnThenOff(pin_size_t pin, int onTime) {
+  digitalWrite(pin, HIGH);
+  delay(onTime);
+  digitalWrite(pin, LOW);
 }
