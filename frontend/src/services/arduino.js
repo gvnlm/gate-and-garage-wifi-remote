e894deff1993952089e08547fc56d2b9d6f81bd4
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-05-11 12:47:02 +1000

    Log error message to console on request failure

--- frontend/src/services/arduino.js
+++ frontend/src/services/arduino.js
@@ -4,11 +4,31 @@ const WIFI_IP = import.meta.env.VITE_WIFI_IP;
 const BASE_URL = `http://${WIFI_IP}`;
 
 const openGate = async () => {
-  axios.get(`${BASE_URL}/gate`);
+  try {
+    await axios.get(`${BASE_URL}/gate`);
+  } catch (error) {
+    if (error.code === 'ERR_NETWORK') {
+      console.log(
+        `Failed to open gate: Arduino server could not be reached. Ensure it is running and the request is to the correct IP.`
+      );
+    } else {
+      console.log('Failed to open gate:', error);
+    }
+  }
 };
 
 const openGarage = async () => {
-  axios.get(`${BASE_URL}/garage`);
+  try {
+    await axios.get(`${BASE_URL}/garage`);
+  } catch (error) {
+    if (error.code === 'ERR_NETWORK') {
+      console.log(
+        `Failed to open gate: Arduino server could not be reached. Ensure it is running and the request is to the correct IP.`
+      );
+    } else {
+      console.log('Failed to open gate:', error);
+    }
+  }
 };
 
 export default { openGate, openGarage };
